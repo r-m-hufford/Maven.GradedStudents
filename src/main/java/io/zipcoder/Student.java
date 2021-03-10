bfe7821b8b30062fe38ca443aeb759819bc6e134
r-m-hufford <ryan@rmhufford.com>
2021-03-10 08:29:48 -0500

    student complete

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,7 +1,6 @@
 package io.zipcoder;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Student {
     private String firstName;
@@ -46,4 +45,49 @@ public class Student {
         return formattedGrades;
     }
 
+    public void addExamScore(double examScore) {
+        this.examScores.add(examScore);
+    }
+
+    public void setExamScores(int examNumber, double newScore) {
+        this.examScores.set(examNumber, newScore);
+    }
+
+    public String getAverageExamScore() {
+        double sumOfGrades = 0;
+
+        for (Double grade : this.examScores) {
+            sumOfGrades += grade;
+        }
+
+        double averageOfGrades = sumOfGrades / this.examScores.size();
+        //String output = "\\`\\`\\`\n" + averageOfGrades + "\n\\`\\`\\`";
+        String border = "\\`\\`\\`";
+        String output = String.format("%s\n %.1f\n%s", border,averageOfGrades,border);
+        return output;
+    }
+
+    @Override
+    public String toString() {
+        String border = "\\`\\`\\`";
+
+        double sumOfGrades = 0;
+        for (Double grade : this.examScores) {
+            sumOfGrades += grade;
+        }
+        double averageOfGrades = sumOfGrades / this.examScores.size();
+
+        String formattedGrades = "";
+        for (int i = 0; i < this.examScores.size(); i++) {
+            formattedGrades += "    Exam " + (i+1) + " -> " + this.examScores.get(i);
+            if (i < this.examScores.size()-1) {
+                formattedGrades += "\n";
+            }
+        }
+
+        String output = String.format("%s\nStudent Name: %s %s\n> Average Score: %s\n> Exam Scores:\n%s\n%s"
+                , border, this.getFirstName(), this.getLastName(), averageOfGrades, formattedGrades, border);
+
+        return output;
+    }
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -64,8 +64,69 @@ public class StudentTest {
         //when
         String output = jack.getExamScores();
         //then
-        System.out.println(output);
         Assert.assertEquals("Exam 1 -> 57.0\nExam 2 -> 34.0\nExam 3 -> 62.0\nExam 4 -> 55.0", output);
+
+    }
+
+    @Test
+    public void addExamScoreTest() {
+        //given
+        Student jason = new Student("jason", "vorhees", new ArrayList<>());
+        //when
+        jason.addExamScore(100.0);
+        String output = jason.getExamScores();
+
+        //then
+        System.out.println(output);
+        Assert.assertEquals(output, "Exam 1 -> 100.0");
+    }
+
+    @Test
+    public void setExamScoreTest() {
+        //given
+        Student freddy = new Student("freddy", "krueger", new ArrayList<>());
+        freddy.addExamScore(100.0);
+        freddy.setExamScores(0,24.0);
+
+        //when
+        String output = freddy.getExamScores();
+
+        //then
+        System.out.println(output);
+        Assert.assertEquals(output, "Exam 1 -> 24.0");
+    }
+
+    @Test
+    public void averageOfGradesTest() {
+        ArrayList<Double> normansGrades = new ArrayList<Double>();
+        normansGrades.add(72.0);
+        normansGrades.add(91.0);
+        normansGrades.add(82.0);
+        normansGrades.add(87.0);
+        Student norman = new Student("norman", "bates",normansGrades);
+
+        //when
+        String output = norman.getAverageExamScore();
+        //then
+        System.out.println(output);
+        Assert.assertEquals("\\`\\`\\`\n 83.0\n\\`\\`\\`", output);
+
+    }
+
+    @Test
+    public void studentSummaryTest() {
+        ArrayList<Double> normansGrades = new ArrayList<Double>();
+        normansGrades.add(72.0);
+        normansGrades.add(91.0);
+        normansGrades.add(82.0);
+        normansGrades.add(87.0);
+        Student norman = new Student("Norman", "Bates",normansGrades);
+
+        //when
+        String output = norman.toString();
+        //then
+        System.out.println(output);
+        Assert.assertEquals(output, output);
 
     }
 }
