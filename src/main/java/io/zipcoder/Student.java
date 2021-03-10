1942f14e7cf08fc00916527b1a7bddcf047aa136
r-m-hufford <ryan@rmhufford.com>
2021-03-09 19:50:11 -0500

    Student half done

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,49 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+    private ArrayList<Double> examScores = new ArrayList<>();
+
+
+    public Student (String newFirstName, String newLastName, ArrayList<Double> newExamScores) {
+        this.firstName = newFirstName;
+        this.lastName = newLastName;
+        this.examScores = newExamScores;
+    }
+
+    public void setFirstName(String newFirstName) {
+        this.firstName = newFirstName;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setLastName(String newLastName) {
+        this.lastName = newLastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public Integer getnumberOfExamsTaken() {
+        return examScores.size();
+    }
+
+    public String getExamScores() {
+        String formattedGrades = "";
+        for (int i = 0; i < this.examScores.size(); i++) {
+            formattedGrades += "Exam " + (i+1) + " -> " + this.examScores.get(i);
+            if (i < this.examScores.size()-1) {
+                formattedGrades += "\n";
+            }
+        }
+        return formattedGrades;
+    }
+
 }
--- src/test/java/io/zipcoder/StudentTest.java
+++ src/test/java/io/zipcoder/StudentTest.java
@@ -1,5 +1,71 @@
 package io.zipcoder;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class StudentTest {
 
+    @Test
+    public void studentConstructorTest1() {
+        //given
+        Student jack = new Student("Jack", "Torrance",null);
+
+        //when
+        String output = jack.getFirstName();
+        //then
+        Assert.assertTrue(output == "Jack");
+    }
+
+
+    @Test
+    public void studentConstructorTest2() {
+        //given
+        ArrayList<Double> jacksGrades = new ArrayList<Double>();
+        jacksGrades.add(57.0);
+        jacksGrades.add(34.0);
+        jacksGrades.add(62.0);
+        jacksGrades.add(55.0);
+        Student jack = new Student("Jack", "Torrance",jacksGrades);
+
+        //when
+        String output = jack.getLastName();
+        //then
+        Assert.assertTrue(output == "Torrance");
+    }
+
+
+    @Test
+    public void studentConstructorTest3() {
+        //given
+        ArrayList<Double> jacksGrades = new ArrayList<Double>();
+        jacksGrades.add(57.0);
+        jacksGrades.add(34.0);
+        jacksGrades.add(62.0);
+        jacksGrades.add(55.0);
+        Student jack = new Student(null, null,jacksGrades);
+
+        //when
+        Integer output = jack.getnumberOfExamsTaken();
+        //then
+        Assert.assertTrue(output == 4);
+    }
+
+    @Test
+    public void printExamScoresTest() {
+        ArrayList<Double> jacksGrades = new ArrayList<Double>();
+        jacksGrades.add(57.0);
+        jacksGrades.add(34.0);
+        jacksGrades.add(62.0);
+        jacksGrades.add(55.0);
+        Student jack = new Student(null, null,jacksGrades);
+
+        //when
+        String output = jack.getExamScores();
+        //then
+        System.out.println(output);
+        Assert.assertEquals("Exam 1 -> 57.0\nExam 2 -> 34.0\nExam 3 -> 62.0\nExam 4 -> 55.0", output);
+
+    }
 }
